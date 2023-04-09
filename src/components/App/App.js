17a1fc467e9e8301c52509248e0120ca9fe3c494
Max Kokarev <makap5545@gmail.com>
2023-04-09 14:33:39 +0300

    Homework 6th Lesson React

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+
+import RegistrationForm from '../RefistrationForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <h1 className='animated'>
+          Homework 5
+        </h1>           
+        <RegistrationForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
